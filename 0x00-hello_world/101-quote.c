b56e0ffc29262776234d65702c4aed4cf8eaa631
mwanyambu <jmwanyambu@gmail.com>
2023-02-16 14:51:11 -0800

    other ways to print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  */
 int main(void) 
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 
 	return (1); 
 }
