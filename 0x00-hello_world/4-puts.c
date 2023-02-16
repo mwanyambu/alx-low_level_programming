3858fe8841e9fff57af39b55d6291c45c824b612
mwanyambu <jmwanyambu@gmail.com>
2023-02-16 13:16:54 -0800

    first c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-serves as the starting point for program execution
+ * Return: This returns 0
+ */
+int main(void)
+{
+
+char message = "Programming is like building a multilingual puzzle"
+/*initialize message */
+
+puts(message)
+return (0);
+
+}
