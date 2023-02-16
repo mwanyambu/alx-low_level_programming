f4c28a91dfb08dcd5688a622d9503a0dd5aad08f
mwanyambu <jmwanyambu@gmail.com>
2023-02-16 14:57:25 -0800

    other ways to print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,14 @@
  * main - prints inputs to standart output
  * Return: returns 1
  */
-int main(void) 
+
+int main(void)
 {
-	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	
 
 	return (1); 
 }
