2c3cf472d074d9a9d75cdef6940f147c3a38fe43
mwanyambu <jmwanyambu@gmail.com>
2023-02-16 14:01:26 -0800

    displaying the sizes of differnt data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - serves as the starting point for program execution
+ * Return : returns 0
+ */
+
+int main(void)
+{
+	printf("Size of char : %d", sizeof(char));
+	printf("Size of int : %d",  sizeof(int));
+	printf("Size of long : %d", sizeof(long));
+	printf("Size of long long : %d", sizeof(long long));
+	printf("Size of float : %d", sizeof(float));
+
+	return (0);
+}
+
