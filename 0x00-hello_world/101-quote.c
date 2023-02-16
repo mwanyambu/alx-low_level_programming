67ffb9f9a19c6c203b39547b506557b982e3226f
mwanyambu <jmwanyambu@gmail.com>
2023-02-16 14:49:44 -0800

    other ways to print

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints inputs to standart output
+ * Return: returns 1
+ */
+int main(void) 
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (1); 
+}
