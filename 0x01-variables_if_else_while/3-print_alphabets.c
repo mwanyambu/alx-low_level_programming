74eb49199b3d441c6fecc50bdd7a6f290752ac5e
mwanyambu <jmwanyambu@gmail.com>
2023-02-17 15:13:23 -0800

    change case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(letter);
+
+	for alpha= 'A'; alpha<= 'Z'; alpha ++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
