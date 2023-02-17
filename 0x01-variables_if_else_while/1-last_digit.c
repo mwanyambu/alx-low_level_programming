bfb7227ef60f5ed86c42667c053ea60aad02053a
mwanyambu <jmwanyambu@gmail.com>
2023-02-17 14:17:42 -0800

    print last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/**
+ * more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - prints last digit
+ * Return: returns 0
 int main(void)
 {
 	int n;
