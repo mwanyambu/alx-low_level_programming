7653ce3b8de820943b39d496d3c202eb7cc1682d
mwanyambu <jmwanyambu@gmail.com>
2023-02-17 12:34:15 -0800

    my first if else

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - initiates printf
+ * Return: returns 0
+ */
+int main(void)
+{
+	int n;
+
+	printf("please enter an integer n: ");
+
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	else if (n == 0)
+	{
+		printf("is zero\n");
+	}
+	else
+	{
+		printf("is negative\n")
+	}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+0-positive_or_negative.c displays if else statements
