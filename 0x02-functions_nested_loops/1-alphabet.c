5bb264c207d01c6132cd160ebd86da9e0646f308
mwanyambu <jmwanyambu@gmail.com>
2023-02-21 14:03:57 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - displays the aphabet in lowercase
+ * Return:returns void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	putchar(c);
+
+}
+_putchat('\n');
