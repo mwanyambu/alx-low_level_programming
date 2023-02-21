dbfb2f3a71af60af1bebd66a31533dfa7f560f06
mwanyambu <jmwanyambu@gmail.com>
2023-02-21 15:48:06 -0800

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints all the alphabet 10 times in lowercase
+ * Return: return 1
+ */
+void print_alphabet_x10(void)
+{
+	char alpha, num;
+
+	for (num = 0; num <= 10; num++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
