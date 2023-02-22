02b3c81e92582ccb3774416f08cd615b92a9ffd3
mwanyambu <jmwanyambu@gmail.com>
2023-02-22 14:05:11 -0800

    98th fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -23,22 +23,25 @@ int main(void)
 			y = z;
 
 		}
-		else if (boolean2)
+		else
 		{
-			y1 = x % 1000000000;
-			z1 = x % 1000000000;
-			x = x / 1000000000;
-			y = y / 1000000000;
-			boolean2 = 0;
+			if (boolean2)
+			{
+				y1 = x % 1000000000;
+				z1 = x % 1000000000;
+				x = x / 1000000000;
+				y = y / 1000000000;
+				boolean2 = 0;
+			}
+			x1 = (y1 + z1);
+			z = (x + y + (x1 / 1000000000));
+			printf("%ld", z);
+			printf("%ld", x1 % 1000000000);
+			x = y;
+			y1 = z1;
+			y = z;
+			z1 = (x1 % 1000000000);
 		}
-		x1 = (y1 + z1);
-		z = (x + y + (x1 / 1000000000));
-		printf("%ld", z);
-		printf("%ld", x1 % 1000000000);
-		x = y;
-		y1 = z1;
-		y = z;
-		z1 = (x1 % 1000000000);
 		if (((x + y) < 0) && boolean == 1)
 		boolean = 0;
 	}
