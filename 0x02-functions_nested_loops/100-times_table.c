f00bda5f3390ec8af76c5897d774b11ee8149f27
mwanyambu <jmwanyambu@gmail.com>
2023-02-22 12:53:18 -0800

    print multiplicatio table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the times table
+ * @n: used to generate integers
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(48);
+			for (j = 1; j <= n; j++)
+			{
+				k = i * j;
+				_putchar(44);
+				_putchar(32);
+				if (k <= 9)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(k + 48);
+				}
+				else if (k <= 99)
+				{
+					_putchar(32);
+					_putchar((k / 10) + 48);
+					_putchar((k % 10) + 48);
+				}
+				else
+				{
+					_putchar(((k / 100) % 10) + 48);
+					_putchar(((k / 10) % 10) + 48);
+					_putchar((k % 10) + 48);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+
