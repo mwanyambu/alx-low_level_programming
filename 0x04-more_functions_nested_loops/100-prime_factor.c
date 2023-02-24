7f669a80ac10324c76f54a47c1d938822b080fd6
mwanyambu <jmwanyambu@gmail.com>
2023-02-23 17:44:27 -0800

    advanced

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the largest prime
+ * Return: 0
+ */
+int main(void)
+{
+	long i maxj;
+	long number = 612852475143;
+	double square = sqrt(number);
+
+	for (i = 1; i <= square; i++)
+	{
+		if (number % i == 0)
+		{
+			maxj = number / i;
+		}
+	}
+	printf("%d\n", maxj);
+	return (0);
+}
+
+
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - prints an integer
+ * @n: the printer
+ */
+void print_number(int n)
+{
+	unsigned int y = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		y = n;
+		_putchar('-');
+	}
+	y /= 10;
+	if (y != 10)
+		print_number(y);
+	_putchar((unsigned int) n % 10 + '0');
+}
+
