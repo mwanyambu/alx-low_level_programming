e8409a6a85af04f6cbaed86ca249a09635d7835a
mwanyambu <jmwanyambu@gmail.com>
2023-02-27 15:46:12 -0800

    reverse str

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,15 +7,15 @@
 void rev_string(char *s)
 {
 	int len = 0, index = 0;
-	char string;
+	char st;
 
 	while (s[index++])
 	len++;
 	
 	for (index = len -1; index >= len / 2; index--)
 	{
-		string = s[index];
+		st = s[index];
 		s[index] = s[len - index -1];
-		s[len - index -1] = strimg;
+		s[len - index -1] = st;
 	}
 }
