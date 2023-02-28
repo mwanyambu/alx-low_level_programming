ee888491290a4569e48b4389af767eb343a288d9
mwanyambu <jmwanyambu@gmail.com>
2023-02-27 16:20:19 -0800

    copy strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,14 +3,14 @@
 /**
  * _strcpy -  copies contents of a string
  * @dest: destination
- * src: source
+ * @src: source
  * Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
 	int x;
 
-	for (x = 0;src[x] != '\0'; x++)
+	for (x = 0; src[x] != '\0'; x++)
 	{
 		dest[x] = src[x];
 	}
