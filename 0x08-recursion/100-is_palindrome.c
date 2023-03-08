4e9f85165687e6966bf7de15d479051baa138f44
mwanyambu <jmwanyambu@gmail.com>
2023-03-08 15:27:35 -0800

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * str_len - returns length of string
+ * @z: string
+ * Return: string length
+ */
+int str_len(char *z)
+{
+	if (z == '\0')
+	{
+		return (0);
+	else
+		return (1+str_len(z + 1));
+}
+/**
+ * _strcmp - compares string
+ * @s: string
+ * @t: int
+ * @u: int
+ */
+int _strcpy(char *s, int t, int u)
+{
+	if (*(s + t) == *(s + u))
+	{
+		if (t == u || t == u + 1)
+			return (1);
+		return (0 + _strcpy(s, t + 1, u -1));
+	}
+	return (0);
+}
+
+
+
+/**
+ * is_palindrome - returns boolean
+ * @s: input
+ * Return: 1 if palindrome else 0
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (_strcpy(s, 0, str_len(z) - 1));
+
+
