9739a1cc1f366368c1eb8a7a3fcca948578c2904
mwanyambu <jmwanyambu@gmail.com>
2023-03-15 13:03:06 -0700

    free grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * free_grid - frees a space and allocates a grid
  * @grid: 2d grid
@@ -11,7 +12,7 @@ void free_grid(int **grid, int height)
 
 {
 	int i;
-	
+
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
