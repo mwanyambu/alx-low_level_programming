a37f008e38498c77fc2e03bd9823747fb1ab8f90
mwanyambu <jmwanyambu@gmail.com>
2023-03-15 06:39:58 -0700

    string concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,11 +24,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = " ";
 	}
 	if (s2 == NULL)
 	{
-		return (NULL);
+		s2 = " ";
 	}
 	for (i = 0; s1[i] || s2[i]; i++)
 	{
