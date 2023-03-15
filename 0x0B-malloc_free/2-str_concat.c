36f002221594caac96810eb2eb36de9eab6e1189
mwanyambu <jmwanyambu@gmail.com>
2023-03-15 06:37:41 -0700

    string concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,6 +7,14 @@
  * @s2: string 2
  * Return: pointer
  */
+char *str_concat(char *s1, char *s2);
+/**
+ * str_concat - joins two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Description: concatnates s1 and s2
+ * Return: conc
+ */
 char *str_concat(char *s1, char *s2)
 {
 	char *conc;
