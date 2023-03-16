404a492ff375db634778c22f15920545090b037c
mwanyambu <jmwanyambu@gmail.com>
2023-03-16 15:23:03 -0700

    array range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	{
 		return (0);
 	}
-	range = malloc((max - min - 1) * sizeof(int));
+	range = malloc((max - min + 1) * sizeof(int));
 	if (!range)
 	{
 		return (0);
