27a52b4106537a5a41ff880442fb9ea1ba0fd3fa
mwanyambu <jmwanyambu@gmail.com>
2023-05-09 14:27:09 -0700

    file operations

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,8 +24,7 @@ char *_buffer(char *file)
  */
 int main(int argc, char **argv)
 {
-	int source, destination;
-	ssize_t rd, wr;
+	int source, destination, rd, wr, csrc, cdest;
 	char *buffer;
 
 	if (argc != 3)
@@ -34,37 +33,34 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 	source = open(argv[1], O_RDONLY);
-	destination = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0644);
+	destination = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	buffer = malloc(sizeof(char) * 1024);
 	rd = read(source, buffer, 1024);
 	wr = write(destination, buffer, rd);
-	if (source == -1 || rd == -1)
+	csrc = close(source);
+	cdest = close(destination);
+	if (source < 0 || rd < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: can't read to %s\n", argv[1]);
-		free(buffer);
+		dprintf(STDERR_FILENO, "Error: Can't read to %s\n", argv[1]);
 		exit(98);
 	}
 	while (rd > 0)
 	{
-		if (destination == -1 || wr == -1)
+		if (destination < 0 || wr != rd)
 		{
-			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(source);
 			exit(99);
 		}
 	}
-	if (close(source) == -1)
+	if (csrc < 0 || cdest < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close file %d\n", source);
-		exit(100);
-	}
-	if (close(destination) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close file %d\n", destination);
+		if (csrc < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close file %d\n", source);
+		if (cdest < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close file %d\n", destination);
 		exit(100);
 	}
 	free(buffer);
-	close(source);
-	close(destination);
 	return (0);
 }
