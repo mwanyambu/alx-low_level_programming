db3ff21fac5bfdb02dc9ecee0cfaaf08accbea81
mwanyambu <jmwanyambu@gmail.com>
2023-05-10 13:30:16 -0700

    file copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	source = open(argv[1], O_RDONLY);
