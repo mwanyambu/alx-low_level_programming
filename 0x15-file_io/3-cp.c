753e2038107099c26a1be2396076ce0424b0eb5a
mwanyambu <jmwanyambu@gmail.com>
2023-05-10 12:44:18 -0700

    file cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,31 +1,14 @@
 #include "main.h"
-/** 
- * _buffer - allocates memory for a buffer
- * @file: filename
- * Return: buffer
- 
-char *_buffer(char *file)
-{
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: can't write to %s\n", file);
-		exit(99);
-	}
-	return (buffer);
-} */
 /**
  * main - copies contents of one file to the other
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 1 on success
+ * Return: 0 on success
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int source, destination, rd, wr, csrc, cdest;
-	char *buffer;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
@@ -33,26 +16,30 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 	source = open(argv[1], O_RDONLY);
-	destination = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	buffer = malloc(sizeof(char) * 1024);
-	rd = read(source, buffer, 1024);
-	wr = write(destination, buffer, rd);
-	csrc = close(source);
-	cdest = close(destination);
-	if (source < 0 || rd < 0)
+	if (source == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read to %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	while (rd > 0)
+	destination = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	rd = read(source, buffer, 1024);
+	if (rd > 0)
 	{
-		if (destination < 0 || wr != rd)
+		wr = write(destination, buffer, rd);
+		if (destination == -1 || wr != rd)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(source);
 			exit(99);
 		}
 	}
+	if (rd < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	csrc = close(source);
+	cdest = close(destination);
 	if (csrc < 0 || cdest < 0)
 	{
 		if (csrc < 0)
@@ -61,6 +48,5 @@ int main(int argc, char **argv)
 			dprintf(STDERR_FILENO, "Error: Can't close file %d\n", destination);
 		exit(100);
 	}
-	free(buffer);
 	return (0);
 }
