7f0b84f0a83ecc47f4c1cb8c120dab57d604c269
mwanyambu <jmwanyambu@gmail.com>
2023-08-17 15:52:27 -0700

    doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,18 +19,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current = *h;
 	for (j = 0; j < idx - 1; j++)
 	{
-		if (current ==  NULL)
+		if (current == NULL)
 			return (NULL);
 		current = current->next;
 	}
 	if (current == NULL)
 		return (NULL);
 
-	if (current->next ==  NULL)
+	if (current->next == NULL)
 		return (add_dnodeint_end(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node ==  NULL)
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
