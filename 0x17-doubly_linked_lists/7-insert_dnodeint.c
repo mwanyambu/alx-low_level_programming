4271169bc24f6caf1a807e6affbe1254589fae07
mwanyambu <jmwanyambu@gmail.com>
2023-08-17 16:17:05 -0700

    doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = current->next;
 	new_node->prev = current;
-	current->next->prev = new_node;
+	if (current->next == new_node)
+		current->next->prev = new_node;
 	current->next = new_node;
 
 	return (new_node);
