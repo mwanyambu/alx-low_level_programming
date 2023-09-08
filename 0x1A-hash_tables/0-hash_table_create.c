6b8ceb4182685c8ac5c0f64c45b7a0667653ba80
mwanyambu <jmwanyambu@gmail.com>
2023-09-08 16:57:31 -0700

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,10 @@
 #include "hash_tables.h"
+#include <stdlib.h>
 
 /**
  * hash_table_create - creates a new hash table
  * @size: size of the array
- * Return: pointer to new hash table esle NULL
+ * Return: pointer to new hash table else NULL
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
