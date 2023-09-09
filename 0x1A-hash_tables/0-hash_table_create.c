9d2920e2c1ce9d200a266da14603375211f86fee
mwanyambu <jmwanyambu@gmail.com>
2023-09-08 17:21:59 -0700

    create table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-#include <stdlib.h>
 
 /**
  * hash_table_create - creates a new hash table
@@ -9,7 +8,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *nw_htable;
-	unsigned long int x;
 
 	nw_htable = malloc(sizeof(hash_table_t));
 
@@ -21,10 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (nw_htable->array == NULL)
 	{
-		free(nw_htable);
 		return (NULL);
 	}
-	for (x = 0; x < size; x++)
-		nw_htable->array[x] = NULL;
 	return (nw_htable);
 }
