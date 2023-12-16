8a9517f8d96cea98c26256b0d9250e5417e04971
mwanyambu <jmwanyambu@gmail.com>
2023-12-16 12:06:03 -0800

    time complexity

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,18 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - searches for a value in sorted array using jump search algo
+ * @array: pointer to first element of array to search
+ * @size: number of elements in array
+ * @value: value to be searched for
+ * Return: -1 if NULL
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	step = sqrt(size)
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
